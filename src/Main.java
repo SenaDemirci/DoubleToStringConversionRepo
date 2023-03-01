6fbcdaf62cf7d956c98269f4455a726254954fa8
SenaDemirci <senad1373@hotmail.com>
2023-03-01 12:39:44 +0300

    Sena Demirci

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Please enter a double number: " );
+
+        Scanner in = new Scanner(System.in);
+
+        String s = in.nextLine();
+
+        // Converting Double data to String data
+        String output = String.valueOf(s);
+
+        System.out.println("Double to string conversion: " + output);
     }
 }
