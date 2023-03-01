82d8396c534577f444c55b017f276a81dc7bd3d3
SenaDemirci <senad1373@hotmail.com>
2023-03-01 12:26:35 +0300

    Sena Demirci

---
+++ src/Converter.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Converter {
+    public static void main(String[] args) {
+
+        System.out.println("Please enter a double number: " );
+
+        Scanner in = new Scanner(System.in);
+
+        String s = in.nextLine();
+
+        String output = String.valueOf(s); //Converting Double data to String data
+
+        System.out.println("Double to string conversion: " + output);
+
+    }
+}
